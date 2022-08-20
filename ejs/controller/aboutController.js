0e3b770a786a738d572107c39b9e14f3a9b394e6
benjaminangafua1997 <benjaminarkutl2017@gmail.com>
2022-08-20 23:22:42 +0000

    Updated version 0.002

--- ejs/controller/aboutController.js
+++ ejs/controller/aboutController.js
@@ -1,9 +1,23 @@
-const regForm = ((req,res)=>res.render('index') )
+const { json } = require("express");
+const fs = require("fs");
+const path = "data.json"; 
+
+const regForm = ((req,res)=>res.render('index') );
 
 const getForm = ((req,res)=>{
+    
+    let parseData =[];
+    fs.readFile(path, { flag: 'r' }, (err, data)=>{
+        parseData.push(JSON.parse(data))
+        
+    })
+
+    console.log(parseData);
+
+    fs.writeFile(path, JSON.stringify(parseData.toString()), err=>console.error({err}))
+
     res.json(req.body)
-    console.log(req.body);
-})
+}) 
 
 
 module.exports = {
